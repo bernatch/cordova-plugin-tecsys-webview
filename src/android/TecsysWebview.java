9e9e6389618dc0c9695cbdec7672073a489b8f79
bernatch <nicolas.bernatchez@tecsys.com>
2020-05-19 10:13:57 -0400

    Update TecsysWebview.java

--- src/android/TecsysWebview.java
+++ src/android/TecsysWebview.java
@@ -42,7 +42,7 @@ public class TecsysWebview extends CordovaPlugin {
         }
 	    
 	if (action.equals("test")) {
-	    //WebView cordovaWebview = new WebView(cordova.getActivity());
+	    WebView cordovaWebview = new WebView(cordova.getActivity());
             //Toast.makeText(cordovaWebview.getContext(), "This is a test", Toast.LENGTH_SHORT).show();
             return true;
         }
