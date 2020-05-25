7bf8773d60fdc669b392524865cd1ecf3b74635e
bernatch <nicolas.bernatchez@tecsys.com>
2020-05-25 09:17:15 -0400

    Update TecsysWebview.java

--- src/android/TecsysWebview.java
+++ src/android/TecsysWebview.java
@@ -86,6 +86,7 @@ public class TecsysWebview extends CordovaPlugin {
 		    final JavaScriptInterface jsInterface = new JavaScriptInterface(cordova.getActivity());
 		    cordovaWebview.getSettings().setJavaScriptEnabled(true);
         	    cordovaWebview.addJavascriptInterface(jsInterface, "TECSYS");
+		    Log.d(TAG, "jsInterface setup");
 		}
 	    };
 	    this.cordova.getActivity().runOnUiThread(runnable);
