4ddf7574df1426c413ded0d5b7e95e3dac33a7fc
bernatch <nicolas.bernatchez@tecsys.com>
2020-06-01 14:44:41 -0400

    Update TecsysJsInterface.java

--- src/android/TecsysJsInterface.java
+++ src/android/TecsysJsInterface.java
@@ -83,6 +83,7 @@ public class TecsysJsInterface implements OnInitListener {
 	   callbackContext_ = callbackContext;
 	   tts_ = new TextToSpeech(context, this);
 		
+		/*
 		Log.d(TAG, "TextToSpeech SUCCESS");
 			// warm up the tts engine with an empty string
 			HashMap<String, String> ttsParams = new HashMap<String, String>();
@@ -94,6 +95,7 @@ public class TecsysJsInterface implements OnInitListener {
 			tts_.speak("", TextToSpeech.QUEUE_FLUSH, ttsParams);
 
 			ttsInitialized_ = true;
+			*/
 	}
 
 	@JavascriptInterface
